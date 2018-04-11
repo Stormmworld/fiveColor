699f24c3e3b0bba2b669cf7e9411aa1c483a9af1
Steve Corrado <steve.corrado@macys.com>
2018-04-11 16:24:48 -0400

    e

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -6,3 +6,29 @@ export const updateDeck = () => ({
 export const resetState = () => ({
   type: RESET_STATE
 });
+export function ApiGetTeams() {
+  return dispatch => {
+      return axios.get("http://elementalserver.servehttp.com/NFLScheduleApi/api/teams")
+      .then((response) => {
+          dispatch({
+              type: "ApiGetTeams",
+              payload: response.data
+          })
+      })
+      .catch((error) => {
+          dispatch ({
+              type: "axiosException",
+              payload: error,
+              customMessage: "Error while getting teams from api call"
+          })
+      })
+  }
+} 
+
+// fetch('http://example.com/movies.json')
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(myJson) {
+//     console.log(myJson);
+//   }); 

4ce61534e011cc4c3d30866c379b5cf2484100d1
Stormmworld <steven.corrado@gmail.com>
2018-04-22 20:12:53 -0400

    testing

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,34 @@ class App extends Component {
   constructor(props) {
     super(props);
     props.retrieveDeck(1);
-};
+
+      this.state = {
+        nick: '',
+        message: '',
+        messages: [],
+        hubConnection: null,
+      };
+}
+
+componentDidMount = () => {
+  const nick = window.prompt('Your name:', 'John');
+  const signalR = require("@aspnet/signalr");
+
+  const hubConnection = new new signalR.HubConnection('/chat');;
+
+  this.setState({ hubConnection, nick }, () => {
+    this.state.hubConnection
+      .start()
+      .then(() => console.log('Connection started!'))
+      .catch(err => console.log('Error while establishing connection :('));
+
+    this.state.hubConnection.on('sendToAll', (nick, receivedMessage) => {
+      const text = `${nick}: ${receivedMessage}`;
+      const messages = this.state.messages.concat([text]);
+      this.setState({ messages });
+    });
+  });
+}
 
   render() {
     return (
